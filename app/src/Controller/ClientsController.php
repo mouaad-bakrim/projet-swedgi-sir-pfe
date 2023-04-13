bf290a84cc51660db677c243901f601371125a4e
mouaad-bakrim <72094966+mouaad-bakrim@users.noreply.github.com>
2023-04-13 13:16:19 +0000

    Merge remote-tracking branch 'origin/master'

---
+++ app/src/Controller/ClientsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ClientsController extends AbstractController
+{
+    #[Route('/clients', name: 'app_clients')]
+    public function index(): Response
+    {
+        return $this->render('clients/index.html.twig', [
+            'controller_name' => 'ClientsController',
+        ]);
+    }
+}
