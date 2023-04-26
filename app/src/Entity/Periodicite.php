8bb37d52c4b8cf948bf73af1308304120c52fc1b
RedaBenzi <benzireda96@gmail.com>
2023-04-26 11:33:58 +0100

    Merge remote-tracking branch 'origin/master'

---
+++ app/src/Entity/Periodicite.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PeriodiciteRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PeriodiciteRepository::class)]
+class Periodicite
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $designation = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?float $duree = null;
+
+    #[ORM\Column]
+    private ?bool $direct = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDesignation(): ?string
+    {
+        return $this->designation;
+    }
+
+    public function setDesignation(string $designation): self
+    {
+        $this->designation = $designation;
+
+        return $this;
+    }
+
+    public function getDuree(): ?float
+    {
+        return $this->duree;
+    }
+
+    public function setDuree(?float $duree): self
+    {
+        $this->duree = $duree;
+
+        return $this;
+    }
+
+    public function isDirect(): ?bool
+    {
+        return $this->direct;
+    }
+
+    public function setDirect(bool $direct): self
+    {
+        $this->direct = $direct;
+
+        return $this;
+    }
+}
