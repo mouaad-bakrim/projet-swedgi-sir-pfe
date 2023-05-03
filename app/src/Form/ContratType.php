2787144f0e79ffac1f049db92f1782f287c21655
mouaad-bakrim <72094966+mouaad-bakrim@users.noreply.github.com>
2023-05-03 14:17:58 +0100

    Merge remote-tracking branch 'origin/master'

--- app/src/Form/ContratType.php
+++ app/src/Form/ContratType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Contrat;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +14,10 @@ class ContratType extends AbstractType
     {
         $builder
             ->add('montant')
-            ->add('dateDebut')
+            ->add('dateDebut', DateType::class, [
+                'widget' => 'single_text',
+                'format' => 'yyyy-MM-dd',
+            ])
             ->add('client')
             ->add('Tache')
         ;
