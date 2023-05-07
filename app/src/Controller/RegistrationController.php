2882f1d817690ada81ecab966074d7baf95b1e2a
mouaad-bakrim <72094966+mouaad-bakrim@users.noreply.github.com>
2023-05-07 14:33:56 +0100

    Merge remote-tracking branch 'origin/master'

---
+++ app/src/Controller/RegistrationController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use App\Repository\UserRepository;
+use App\Security\AppCustomAuthenticator;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class RegistrationController extends AbstractController
+{
+    private $registerRepository;
+    private  $entityManager;
+
+    public function __construct(UserRepository $registerRepository, ManagerRegistry $doctrine)
+    {
+        $this->registerRepository = $registerRepository;
+        $this->entityManager = $doctrine->getManager();
+
+    }
+
+    #[Route('/register_app', name: 'index_app')]
+    public function index(): Response
+    {
+        $registers = $this->registerRepository->findAll();
+        return $this->render('registration/index.html.twig', [
+
+            'registers' => $registers,
+        ]);
+    }
+
+
+    #[Route('/register', name: 'app_register')]
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, AppCustomAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+            // do anything else you need here, like send an email
+
+            return $userAuthenticator->authenticateUser(
+                $user,
+                $authenticator,
+                $request
+            );
+        }
+
+        return $this->render('registration/register.html.twig', [
+            'registrationForm' => $form->createView(),
+        ]);
+    }
+
+    //delete
+
+
+    #[Route('/register/delete/{id}', name: 'register_delete')]
+    public function delete(User $register): Response
+    {
+        $this->entityManager->remove($register);
+        $this->entityManager->flush();
+        $this->addFlash(
+            'success',
+            'Your register was removed'
+        );
+
+        return $this->redirectToRoute('index_app');
+
+
+    }
+
+
+
+
+}
